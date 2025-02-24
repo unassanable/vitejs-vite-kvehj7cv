525fa6a75f4bcfa7092d1d2159291e269456180f
unassanable <zavian.alam.s70@kalvium.community>
2025-02-24 11:05:00 +0530

    Fixed BookCard import path

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
 // write the book component code here
+import React from "react";
+
+const BookCard = ({ title, author, cover }) => {
+  return (
+    <div className="book-card">
+      <img src={cover} alt={title} className="book-cover" />
+      <div className="book-info">
+        <h3>{title}</h3>
+        <p>by {author}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
